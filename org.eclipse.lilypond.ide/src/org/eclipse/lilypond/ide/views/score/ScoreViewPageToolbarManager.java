96695fd1ec11e75dd43b7d4e794cf20635703124
thsoft <devnull@localhost>
2009-10-04 15:35:56 +0200

    Implemented zoom Replaced icons with more fancy ones Code improvements

--- org.eclipse.lilypond.ide/src/org/eclipse/lilypond/ide/views/score/ScoreViewPageToolbarManager.java
+++ org.eclipse.lilypond.ide/src/org/eclipse/lilypond/ide/views/score/ScoreViewPageToolbarManager.java
@@ -5,8 +5,12 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ControlContribution;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.lilypond.ide.Activator;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.MouseAdapter;
@@ -16,6 +20,7 @@ import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Scrollable;
 import org.eclipse.swt.widgets.Text;
 
 /**
@@ -23,11 +28,13 @@ import org.eclipse.swt.widgets.Text;
  */
 public class ScoreViewPageToolbarManager {
 
-	private final ScoreViewPage score;
+	protected final ScoreViewPage score;
 
-	private static final String ICON_PATH = "icons/toolbars/Score/";
+	protected static final String ICON_PATH = "icons/toolbars/Score/"; //$NON-NLS-1$
 
-	private static final String ID_PREFIX = ".toolbars.Score.";
+	protected static final String ID_PREFIX = ".toolbars.Score."; //$NON-NLS-1$
+
+	protected final FitToPageAction fitToPageAction = new FitToPageAction();
 
 	public ScoreViewPageToolbarManager(ScoreViewPage score) {
 		super();
@@ -39,6 +46,11 @@ public class ScoreViewPageToolbarManager {
 		toolBarManager.add(new PageCountContribution());
 		toolBarManager.add(new NextPageAction());
 		toolBarManager.add(new LastPageAction());
+		toolBarManager.add(new Separator());
+		toolBarManager.add(new ZoomOutAction());
+		toolBarManager.add(new ZoomInAction());
+		toolBarManager.add(new ZoomToActualSizeAction());
+		toolBarManager.add(fitToPageAction);
 	}
 
 	protected IToolBarManager getToolBarManager() {
@@ -59,7 +71,7 @@ public class ScoreViewPageToolbarManager {
 
 		public FirstPageAction() {
 			setToolTipText("Go To First Page");
-			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "FirstPage.png"));
+			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "FirstPage.png")); //$NON-NLS-1$
 		}
 
 		@Override
@@ -73,7 +85,7 @@ public class ScoreViewPageToolbarManager {
 
 		public PreviousPageAction() {
 			setToolTipText("Go To Previous Page");
-			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "PreviousPage.png"));
+			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "PreviousPage.png")); //$NON-NLS-1$
 		}
 
 		@Override
@@ -92,7 +104,7 @@ public class ScoreViewPageToolbarManager {
 
 		public NextPageAction() {
 			setToolTipText("Go To Next Page");
-			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "NextPage.png"));
+			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "NextPage.png")); //$NON-NLS-1$
 		}
 
 		@Override
@@ -111,7 +123,7 @@ public class ScoreViewPageToolbarManager {
 
 		public LastPageAction() {
 			setToolTipText("Go To Last Page");
-			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "LastPage.png"));
+			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "LastPage.png")); //$NON-NLS-1$
 		}
 
 		@Override
@@ -126,7 +138,7 @@ public class ScoreViewPageToolbarManager {
 		private Text text;
 
 		public CurrentPageContribution() {
-			super(Activator.PLUGIN_ID + ID_PREFIX + "currentPage");
+			super(Activator.PLUGIN_ID + ID_PREFIX + "currentPage"); //$NON-NLS-1$
 		}
 
 		@Override
@@ -186,7 +198,7 @@ public class ScoreViewPageToolbarManager {
 		private Label label;
 
 		public PageCountContribution() {
-			super(Activator.PLUGIN_ID + ID_PREFIX + "pageCount");
+			super(Activator.PLUGIN_ID + ID_PREFIX + "pageCount"); //$NON-NLS-1$
 		}
 
 		@Override
@@ -201,6 +213,112 @@ public class ScoreViewPageToolbarManager {
 			if ((label != null) && !label.isDisposed()) {
 				label.setText(MessageFormat.format("/{0}", score.getPageCount()));
 				label.setToolTipText("Page count");
+			}
+		}
+
+	}
+
+	protected static final float ZOOM_STEP = 0.25f;
+
+	protected static final float ZOOM_IN_FACTOR = 1 + ZOOM_STEP;
+
+	protected static final float ZOOM_OUT_FACTOR = 1 - ZOOM_STEP;
+
+	public class ZoomOutAction extends Action {
+
+		public ZoomOutAction() {
+			setToolTipText("Zoom Out");
+			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "ZoomOut.png")); //$NON-NLS-1$
+		}
+
+		@Override
+		public void run() {
+			fitToPageAction.setChecked(false);
+			score.setZoom(getNewZoom());
+		}
+
+		private float getNewZoom() {
+			return score.getZoom() * ZOOM_OUT_FACTOR;
+		}
+
+		@Override
+		public boolean isEnabled() {
+			return score.isZoomValid(getNewZoom());
+		}
+
+	}
+
+	public class ZoomInAction extends Action {
+
+		public ZoomInAction() {
+			setToolTipText("Zoom In");
+			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "ZoomIn.png")); //$NON-NLS-1$
+		}
+
+		@Override
+		public void run() {
+			fitToPageAction.setChecked(false);
+			score.setZoom(getNewZoom());
+		}
+
+		private float getNewZoom() {
+			return score.getZoom() * ZOOM_IN_FACTOR;
+		}
+
+		@Override
+		public boolean isEnabled() {
+			return score.isZoomValid(getNewZoom());
+		}
+
+	}
+
+	public class ZoomToActualSizeAction extends Action {
+
+		public ZoomToActualSizeAction() {
+			setToolTipText("Zoom To Actual Size");
+			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "ZoomToActualSize.png")); //$NON-NLS-1$
+		}
+
+		@Override
+		public void run() {
+			fitToPageAction.setChecked(false);
+			score.setZoom(1);
+		}
+
+	}
+
+	public class FitToPageAction extends Action {
+
+		private final ControlListener resizeListener = new ControlAdapter() {
+
+			@Override
+			public void controlResized(ControlEvent e) {
+				Scrollable scrollable = (Scrollable)score.getControl();
+				float imageWidth = scrollable.getClientArea().width - scrollable.getVerticalBar().getSize().x;
+				int pageWidth = score.getPageWidth();
+				float widthRatio = imageWidth / pageWidth;
+				float imageHeight = scrollable.getClientArea().height - scrollable.getHorizontalBar().getSize().y;
+				int pageHeight = score.getPageHeight();
+				float heightRatio = imageHeight / pageHeight;
+				score.setZoom(Math.min(widthRatio, heightRatio));
+			}
+
+		};
+
+		public FitToPageAction() {
+			super(null, AS_CHECK_BOX);
+			setToolTipText("Fit To Page");
+			setImageDescriptor(Activator.getImageDescriptor(ICON_PATH + "FitToPage.png")); //$NON-NLS-1$
+		}
+
+		@Override
+		public void setChecked(boolean checked) {
+			super.setChecked(checked);
+			if (checked) {
+				resizeListener.controlResized(null);
+				score.getControl().addControlListener(resizeListener);
+			} else {
+				score.getControl().removeControlListener(resizeListener);
 			}
 		}
 
