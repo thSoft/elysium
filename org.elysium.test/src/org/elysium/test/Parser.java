83fd4f4fd67d9adf0f97a32349e7441d3eb8f531
thsoft <devnull@localhost>
2010-11-09 01:31:39 +0100

    Simplified testing

--- org.elysium.test/src/org/elysium/test/Parser.java
+++ org.elysium.test/src/org/elysium/test/Parser.java
@@ -1,66 +1,18 @@
 package org.elysium.test;
 
-import static org.eclipse.xtext.junit.util.IResourcesSetupUtil.addNature;
-import static org.eclipse.xtext.junit.util.IResourcesSetupUtil.cleanWorkspace;
-import static org.eclipse.xtext.junit.util.IResourcesSetupUtil.createFile;
-import static org.eclipse.xtext.junit.util.IResourcesSetupUtil.waitForAutoBuild;
-import static org.junit.Assert.assertEquals;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.ecore.EValidator;
-import org.eclipse.xtext.junit.util.IResourcesSetupUtil;
-import org.eclipse.xtext.ui.XtextProjectHelper;
-import org.elysium.LilyPondConstants;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.elysium.LilyPondStandaloneSetup;
 
-public class Parser {
+public class Parser extends AbstractXtextTests {
 
-	private static final String PROJECT_NAME = "test";
-
-	private static final String FOLDER_NAME = "test";
-
-	private static final String FOLDER_PATH = PROJECT_NAME + "/" + FOLDER_NAME;
-
-	private static final String FILE_NAME = "test";
-
-	private static final String FILE_PATH = FOLDER_PATH + "/" + FILE_NAME + "." + LilyPondConstants.EXTENSION;
-
-	private static IProject createXtextProject(String name) throws Exception {
-		IProject project = IResourcesSetupUtil.createProject(name);
-		addNature(project.getProject(), XtextProjectHelper.NATURE_ID);
-		return project;
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(LilyPondStandaloneSetup.class);
 	}
 
-	@BeforeClass
-	public static void init() throws Exception {
-		createXtextProject(PROJECT_NAME);
-	}
-
-	@After
-	public void delete() throws Exception {
-		ResourcesPlugin.getWorkspace().getRoot().findMember(FOLDER_PATH).delete(true, new NullProgressMonitor());
-	}
-
-	@AfterClass
-	public static void done() throws Exception {
-		cleanWorkspace();
-	}
-
-	private void isValid(String contents) throws Exception {
-		IFile file = createFile(FILE_PATH, contents);
-		waitForAutoBuild();
-		assertEquals(0, file.findMarkers(EValidator.MARKER, true, IResource.DEPTH_INFINITE).length);
-	}
-
-	@Test
-	public void simple() throws Exception {
-		isValid("{ c' }");
+	public void testSimple() throws Exception {
+		getModel("{ c' }");
 	}
 
 }
