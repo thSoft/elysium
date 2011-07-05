7fb79b61969f82cd803378885a85d038ad106336
thsoft <devnull@localhost>
2011-07-05 13:25:20 +0200

    Updated LilyPond version

--- org.elysium.ui/src/org/elysium/ui/version/LilyPondVersion.java
+++ org.elysium.ui/src/org/elysium/ui/version/LilyPondVersion.java
@@ -10,7 +10,7 @@ public class LilyPondVersion {
 
 	public static String get() {
 		// TODO query version by running LilyPond
-		return "2.12.3"; //$NON-NLS-1$
+		return "2.14.0"; //$NON-NLS-1$
 	}
 
 }
