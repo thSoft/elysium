ea284cc574e8877a0d04fe374e38717c1692acfd
thsoft <devnull@localhost>
2011-07-08 02:17:59 +0200

    Enabled more auto edits in comments

--- org.elysium.ui/src/org/elysium/ui/autoedit/LilyPondAutoEditStrategyProvider.java
+++ org.elysium.ui/src/org/elysium/ui/autoedit/LilyPondAutoEditStrategyProvider.java
@@ -1,7 +1,9 @@
 package org.elysium.ui.autoedit;
 
+import org.eclipse.jface.text.IAutoEditStrategy;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.xtext.ui.editor.autoedit.DefaultAutoEditStrategyProvider;
+import org.eclipse.xtext.ui.editor.model.TerminalsTokenTypeToPartitionMapper;
 import org.elysium.formatting.LilyPondFormatter;
 
 /**
@@ -9,18 +11,27 @@ import org.elysium.formatting.LilyPondFormatter;
  */
 public class LilyPondAutoEditStrategyProvider extends DefaultAutoEditStrategyProvider {
 
+	private void accept(IEditStrategyAcceptor acceptor, IAutoEditStrategy strategy) {
+		acceptor.accept(strategy, IDocument.DEFAULT_CONTENT_TYPE);
+		acceptor.accept(strategy, TerminalsTokenTypeToPartitionMapper.COMMENT_PARTITION);
+		acceptor.accept(strategy, TerminalsTokenTypeToPartitionMapper.SL_COMMENT_PARTITION);
+	}
+
 	@Override
 	protected void configure(IEditStrategyAcceptor acceptor) {
 		configureIndentationEditStrategy(acceptor);
 
 		for (String[] blockKeywordPair : LilyPondFormatter.BLOCK_KEYWORD_PAIRS) {
-			acceptor.accept(singleLineTerminals.newInstance(blockKeywordPair[0], blockKeywordPair[1]), IDocument.DEFAULT_CONTENT_TYPE);
-			acceptor.accept(multiLineTerminals.newInstance(blockKeywordPair[0], null, blockKeywordPair[1]), IDocument.DEFAULT_CONTENT_TYPE);
+			accept(acceptor, singleLineTerminals.newInstance(blockKeywordPair[0], blockKeywordPair[1]));
+			accept(acceptor, multiLineTerminals.newInstance(blockKeywordPair[0], null, blockKeywordPair[1]));
 		}
-		acceptor.accept(singleLineTerminals.newInstance("[", "]"), IDocument.DEFAULT_CONTENT_TYPE); //$NON-NLS-1$ //$NON-NLS-2$
-		acceptor.accept(singleLineTerminals.newInstance("\\(", "\\)"), IDocument.DEFAULT_CONTENT_TYPE); //$NON-NLS-1$ //$NON-NLS-2$
-		acceptor.accept(singleLineTerminals.newInstance("(", ")"), IDocument.DEFAULT_CONTENT_TYPE); //$NON-NLS-1$ //$NON-NLS-2$
-		acceptor.accept(partitionInsert.newInstance("\"", "\""), IDocument.DEFAULT_CONTENT_TYPE);
+		String[][] brackets = new String[][] { { "[", "]" }, //$NON-NLS-1$ //$NON-NLS-2$
+			{ "\\(", "\\)" }, //$NON-NLS-1$ //$NON-NLS-2$
+			{ "(", ")" } }; //$NON-NLS-1$ //$NON-NLS-2$
+		for (String[] blockKeywordPair : brackets) {
+			accept(acceptor, singleLineTerminals.newInstance(blockKeywordPair[0], blockKeywordPair[1]));
+		}
+		accept(acceptor, partitionInsert.newInstance("\"", "\"")); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 }
