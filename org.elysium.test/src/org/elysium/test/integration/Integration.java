fe35c5ecc71871e9e4500b7e04d5b8ec1c32cc3b
thsoft <harmathdenes@gmail.com>
2012-02-07 00:09:07 +0100

    Added static imports

--- org.elysium.test/src/org/elysium/test/integration/Integration.java
+++ org.elysium.test/src/org/elysium/test/integration/Integration.java
@@ -2,14 +2,16 @@ package org.elysium.test.integration;
 
 import static com.google.common.collect.Iterables.concat;
 import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.Math.min;
+import static javax.util.file.FileUtils.readFileAsString;
+import static org.eclipse.emf.common.util.URI.createURI;
 import java.io.File;
 import java.text.MessageFormat;
 import java.util.Collection;
-import javax.util.file.FileUtils;
 import javax.util.file.FilenameExtensionFilter;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.junit.validation.AssertableDiagnostics;
 import org.eclipse.xtext.resource.XtextResource;
 import org.elysium.LilyPondConstants;
@@ -19,23 +21,20 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import com.google.common.base.Predicate;
-import com.google.common.collect.Lists;
 
 @RunWith(value = Parameterized.class)
 public class Integration extends LilyPondTestWithValidator {
 
 	private Iterable<Object> getErrors(String filePath) throws Exception {
-		String model = FileUtils.readFileAsString(filePath);
-		XtextResource resource = doGetResource(getAsStream(model), URI.createURI("test"));
+		String model = readFileAsString(filePath);
+		XtextResource resource = doGetResource(getAsStream(model), createURI("test"));
 		EList<org.eclipse.emf.ecore.resource.Resource.Diagnostic> parseErrors = resource.getErrors();
 		AssertableDiagnostics validationDiagnostics = tester.validate(resource.getContents().get(0));
 		Iterable<Diagnostic> validationErrors = filter(validationDiagnostics.getAllDiagnostics(), new Predicate<Diagnostic>() {
-
 			@Override
 			public boolean apply(Diagnostic input) {
 				return input.getSeverity() == Diagnostic.ERROR;
 			}
-
 		});
 		return concat(parseErrors, validationErrors);
 	}
@@ -48,7 +47,7 @@ public class Integration extends LilyPondTestWithValidator {
 
 	@Parameters
 	public static Collection<Object[]> data() {
-		Collection<Object[]> result = Lists.newArrayList();
+		Collection<Object[]> result = newArrayList();
 		for (File directory : new File[] { new File("init"), new File("snippets") }) {
 			for (File file : directory.listFiles(new FilenameExtensionFilter(LilyPondConstants.EXTENSION))) {
 				result.add(new Object[] { file.getAbsolutePath() });
@@ -59,7 +58,7 @@ public class Integration extends LilyPondTestWithValidator {
 
 	private static int getColumn(String[] lines, int line, int offset) {
 		int lineOffset = 0;
-		for (int i = 0; i < Math.min(lines.length, line); i++) {
+		for (int i = 0; i < min(lines.length, line); i++) {
 			lineOffset += lines[i].length() + 1;
 		}
 		return offset - lineOffset;
@@ -70,7 +69,7 @@ public class Integration extends LilyPondTestWithValidator {
 		Iterable<Object> errors = getErrors(filePath);
 		boolean hasErrors = errors.iterator().hasNext();
 		if (hasErrors) {
-			String[] lines = FileUtils.readFileAsString(filePath).split("\\r?\\n");
+			String[] lines = readFileAsString(filePath).split("\\r?\\n");
 			for (Object error : errors) {
 				if (error instanceof org.eclipse.xtext.diagnostics.Diagnostic) {
 					org.eclipse.xtext.diagnostics.Diagnostic diagnostic = (org.eclipse.xtext.diagnostics.Diagnostic)error;
@@ -86,4 +85,5 @@ public class Integration extends LilyPondTestWithValidator {
 		}
 		assertFalse(hasErrors);
 	}
+
 }
