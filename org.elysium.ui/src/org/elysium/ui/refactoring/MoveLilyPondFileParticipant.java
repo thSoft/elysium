2f7892f94e2754626dd258eeb9291eb05e5036ef
Dénes Harmath <harmathdenes@gmail.com>
2012-08-07 14:34:38 +0200

    When a LilyPond source file is moved, update its references

--- org.elysium.ui/src/org/elysium/ui/refactoring/MoveLilyPondFileParticipant.java
+++ org.elysium.ui/src/org/elysium/ui/refactoring/MoveLilyPondFileParticipant.java
@@ -1,5 +1,6 @@
 package org.elysium.ui.refactoring;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -7,13 +8,13 @@ import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
-import org.eclipse.ltk.core.refactoring.participants.RenameParticipant;
+import org.eclipse.ltk.core.refactoring.participants.MoveParticipant;
 import org.elysium.LilyPondConstants;
 
 /**
- * Updates references to a LilyPond source file when it is renamed.
+ * Updates references to a LilyPond source file when it is moved.
  */
-public class RenameLilyPondFileParticipant extends RenameParticipant {
+public class MoveLilyPondFileParticipant extends MoveParticipant {
 
 	private IFile file;
 
@@ -25,7 +26,7 @@ public class RenameLilyPondFileParticipant extends RenameParticipant {
 
 	@Override
 	public String getName() {
-		return "Rename LilyPond Source File";
+		return "Move LilyPond Source File";
 	}
 
 	@Override
@@ -35,8 +36,8 @@ public class RenameLilyPondFileParticipant extends RenameParticipant {
 
 	@Override
 	public Change createChange(IProgressMonitor pm) throws CoreException, OperationCanceledException {
-		String newName = getArguments().getNewName();
-		return ReferenceUpdater.createChange(file, newName, file.getParent());
+		IContainer destination = (IContainer)getArguments().getDestination();
+		return ReferenceUpdater.createChange(file, file.getName(), destination);
 	}
 
 }
