5c0916d9f4f9a06da32d52f276c502feee1b7878
Dénes Harmath <harmathdenes@gmail.com>
2012-08-09 18:09:28 +0200

    Don't delete compiled files when deleting source file

--- org.elysium.ui/src/org/elysium/ui/refactoring/DeleteFileParticipant.java
+++ org.elysium.ui/src/org/elysium/ui/refactoring/DeleteFileParticipant.java
@@ -1,20 +1,13 @@
 package org.elysium.ui.refactoring;
 
-import static org.elysium.ui.refactoring.RefactoringSupport.ifNotEmpty;
-import static org.elysium.ui.refactoring.RefactoringSupport.isCompiledFrom;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IResourceVisitor;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.ltk.core.refactoring.Change;
-import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 import org.eclipse.ltk.core.refactoring.participants.DeleteParticipant;
-import org.eclipse.ltk.core.refactoring.resource.DeleteResourceChange;
 
 public class DeleteFileParticipant extends DeleteParticipant {
 
@@ -38,20 +31,7 @@ public class DeleteFileParticipant extends DeleteParticipant {
 
 	@Override
 	public Change createChange(IProgressMonitor pm) throws CoreException, OperationCanceledException {
-		final CompositeChange result = new CompositeChange("Delete compiled files");
-		ResourcesPlugin.getWorkspace().getRoot().accept(new IResourceVisitor() {
-			@Override
-			public boolean visit(IResource resource) throws CoreException {
-				if (resource instanceof IFile) {
-					IFile file = (IFile)resource;
-					if (isCompiledFrom(file, sourceFile)) {
-						result.add(new DeleteResourceChange(file.getFullPath(), true));
-					}
-				}
-				return true;
-			}
-		});
-		return ifNotEmpty(result);
+		return null;
 	}
 
 }
