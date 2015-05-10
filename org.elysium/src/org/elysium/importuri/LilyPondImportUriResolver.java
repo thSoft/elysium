8438d44b995cf4396356f4c681b9089e04c888c3
Alexander Nittka <nittka@itemis.de>
2015-05-10 17:01:48 +0200

    issue #23 more flexible default search path

--- org.elysium/src/org/elysium/importuri/LilyPondImportUriResolver.java
+++ org.elysium/src/org/elysium/importuri/LilyPondImportUriResolver.java
@@ -1,12 +1,19 @@
 package org.elysium.importuri;
 
 import static com.google.common.collect.Iterables.transform;
+
 import java.io.File;
+import java.io.FileFilter;
 import java.net.URI;
 import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.scoping.impl.ImportUriResolver;
+
 import com.google.common.base.Function;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
@@ -46,10 +53,33 @@ public class LilyPondImportUriResolver extends ImportUriResolver {
 		return importUri;
 	}
 
+	private static LoadingCache<String, URI> defaultSearchUriCache=CacheBuilder.newBuilder().maximumSize(3).build(new CacheLoader<String, URI>(){
+		@Override
+		public URI load(String key) throws Exception {
+			URI lilypondUri = stringToUri(key);
+			File f=new File(lilypondUri.resolve("../share/lilypond/"));
+			if (f.isDirectory()){
+				for (File subDir : f.listFiles()) {
+					if(subDir.isDirectory()){
+						File[] candidates = subDir.listFiles(new FileFilter() {
+							@Override
+							public boolean accept(File pathname) {
+								return pathname.getName().equals("ly");
+							}
+						});
+						if(candidates.length>0){
+							return candidates[0].toURI();
+						}
+					}
+				}
+			}
+			URI defaultSearchUri = lilypondUri.resolve("../share/lilypond/current/ly/"); //$NON-NLS-1$
+			return defaultSearchUri;
+		}
+	});
+
 	public static URI getDefaultSearchUri(String lilypondPath) {
-		URI lilypondUri = stringToUri(lilypondPath);
-		URI defaultSearchUri = lilypondUri.resolve("../share/lilypond/current/ly/"); //$NON-NLS-1$ // TODO first existing directory instead of 'current'
-		return defaultSearchUri;
+		return defaultSearchUriCache.getUnchecked(lilypondPath);
 	}
 
 	private static URI stringToUri(String lilypondPath) {
