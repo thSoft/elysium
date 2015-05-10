c3569438f390336128bfb220bbdbe44773f6f953
Alexander Nittka <nittka@itemis.de>
2015-05-10 11:55:15 +0200

    #44 allow relavive includes pointing outside a project

--- org.elysium/src/org/elysium/importuri/LilyPondImportUriResolver.java
+++ org.elysium/src/org/elysium/importuri/LilyPondImportUriResolver.java
@@ -1,11 +1,15 @@
 package org.elysium.importuri;
 
 import static com.google.common.collect.Iterables.transform;
+
 import java.io.File;
 import java.net.URI;
 import java.util.List;
+
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.scoping.impl.ImportUriResolver;
+
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
@@ -40,6 +44,20 @@ public class LilyPondImportUriResolver extends ImportUriResolver {
 				File importedFile = new File(resolvedImportUri);
 				if (importedFile.exists()) {
 					return resolvedImportUri.toString();
+				}
+			}
+			//#44 check if resolving relative path yields existing file
+			if(importUri.contains("..")){//!URI.create(importUri).isAbsolute()){
+				org.eclipse.emf.common.util.URI resourceUri = object.eResource().getURI();
+				if(resourceUri!=null && resourceUri.isPlatform()){
+					File base = new File(Platform.getLocation()+resourceUri.toPlatformString(false));
+					if (base.exists()) {
+						URI resolvedImportUri = base.toURI().resolve(importUri);
+						File importedFile = new File(base.toURI().resolve(importUri));
+						if (importedFile.exists()) {
+							return resolvedImportUri.toString();
+						}
+					}
 				}
 			}
 		}
