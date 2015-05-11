1e2e8f1b99387485d21278f4bbbfe53094a790f3
Alexander Nittka <nittka@itemis.de>
2015-05-11 21:11:59 +0200

    #48 allow compilation cancelling

---
+++ org.elysium.ui/src/org/elysium/ui/compiler/CancellableProcessUtils.java
@@ -0,0 +1,72 @@
+package org.elysium.ui.compiler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.util.process.OutputProcessor;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.google.common.base.Throwables;
+
+public class CancellableProcessUtils {
+
+	private CancellableProcessUtils() {
+	}
+
+	/**
+	 * Runs a process synchronously described by the given process builder and
+	 * processes the lines of its output with the given output processor.
+	 */
+	public static void runCancellableProcess(final ProcessBuilder processBuilder, final OutputProcessor outputProcessor, final IProgressMonitor monitor) throws IOException, InterruptedException {
+		final AtomicBoolean done=new AtomicBoolean(false);
+
+		final Process process = processBuilder.start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(!done.get()){
+					if(monitor.isCanceled()){
+						process.destroy();
+						return;
+					}
+					try {
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+		}).start();
+
+		try{
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+					String line;
+					try {
+						while ((line = reader.readLine()) != null) {
+							if (outputProcessor != null) {
+								outputProcessor.processOutput(line);
+							}
+						}
+					} catch (IOException e) {
+						Throwables.propagate(e);
+					}
+				}
+				
+			}).start();
+			process.waitFor();
+		}catch(RuntimeException e){
+			if(e.getCause() instanceof IOException){
+				throw (IOException)e.getCause();
+			}
+			throw e;
+		}finally{
+			done.set(true);
+		}
+	}
+}
