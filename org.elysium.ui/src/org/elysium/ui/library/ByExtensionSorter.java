570466f71e1d2dd6f3db799be844f85dc076ad55
Dénes Harmath <harmathdenes@gmail.com>
2015-08-06 10:46:16 +0200

    Fix #105

--- org.elysium.ui/src/org/elysium/ui/library/ByExtensionSorter.java
+++ org.elysium.ui/src/org/elysium/ui/library/ByExtensionSorter.java
@@ -1,9 +1,12 @@
 package org.elysium.ui.library;
 
 import java.text.Collator;
+
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
+
+import com.google.common.base.Strings;
 
 /**
  * Sorts resources by extension first, then by name, ignoring case.
@@ -19,21 +22,21 @@ public class ByExtensionSorter extends ViewerSorter {
 
 	@Override
 	public int compare(Viewer viewer, Object e1, Object e2) {
-		IResource resource1 = (IResource)e1;
-		IResource resource2 = (IResource)e2;
-		String extension1 = resource1.getFileExtension();
-		if (extension1 == null) {
-			extension1 = ""; //$NON-NLS-1$
-		}
-		String extension2 = resource2.getFileExtension();
-		if (extension2 == null) {
-			extension2 = ""; //$NON-NLS-1$
-		}
-		int result = extension1.compareToIgnoreCase(extension2);
-		if (result == 0) {
-			return resource1.getName().compareToIgnoreCase(resource2.getName());
+		if ((e1 instanceof IResource) && (e2 instanceof IResource)) {
+			IResource resource1 = (IResource)e1;
+			IResource resource2 = (IResource)e2;
+			String extension1 = Strings.nullToEmpty(resource1.getFileExtension());
+			String extension2 = Strings.nullToEmpty(resource2.getFileExtension());
+			int result = extension1.compareToIgnoreCase(extension2);
+			if (result == 0) {
+				String name1 = Strings.nullToEmpty(resource1.getName());
+				String name2 = Strings.nullToEmpty(resource2.getName());
+				return name1.compareToIgnoreCase(name2);
+			} else {
+				return result;
+			}
 		} else {
-			return result;
+			return 0;
 		}
 	}
 
