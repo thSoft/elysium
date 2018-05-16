041cc88ea50495917ecbf462b58bc0c1c2f51613
Alexander Nittka <alex@nittka.de>
2018-05-16 15:50:29 +0200

    disable preferences if Lilypond refactoring is not enabled

--- org.elysium.parent/org.elysium.ui/src/org/elysium/ui/preferences/LilyPondRefactoringPreferencePage.java
+++ org.elysium.parent/org.elysium.ui/src/org/elysium/ui/preferences/LilyPondRefactoringPreferencePage.java
@@ -1,5 +1,8 @@
 package org.elysium.ui.preferences;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.xtext.ui.refactoring.ui.RefactoringPreferencePage;
 
@@ -15,15 +18,41 @@ public class LilyPondRefactoringPreferencePage extends RefactoringPreferencePage
 	@Override
 	protected void createFieldEditors() {
 		super.createFieldEditors();
-		addField(new BooleanFieldEditor(REFACTORING_IS_ENABLED,
-				"Enable Lilypond specific refactorings", getFieldEditorParent()));
-		addField(new BooleanFieldEditor(REFACTORING_DELETE_COMPILED,
-				"Delete compiled (pdf, midi) when deleting source", getFieldEditorParent()));
-		addField(new BooleanFieldEditor(REFACTORING_WARN_DELETE_INCLUDED,
-				"Warn if an indluded file is deleted", getFieldEditorParent()));
-		addField(new BooleanFieldEditor(REFACTORING_WARN_SEARCHPATH,
-				"Warn if a search path is affected by a refactoring", getFieldEditorParent()));
-		addField(new BooleanFieldEditor(REFACTORING_WARN_VARIABLE_INCLUDE,
-				"Warn if a there are variable includes (refactoring not supported)", getFieldEditorParent()));
+		final List<BooleanFieldEditor> dependentFields = new ArrayList<>();
+		BooleanFieldEditor enabledEditor = new BooleanFieldEditor(REFACTORING_IS_ENABLED,
+				"Enable Lilypond specific refactorings", getFieldEditorParent()) {
+			@Override
+			protected void valueChanged(boolean oldValue, boolean newValue) {
+				super.valueChanged(oldValue, newValue);
+				setEnablement(dependentFields, newValue);
+			}
+
+			@Override
+			protected void doLoad() {
+				super.doLoad();
+				setEnablement(dependentFields, getBooleanValue());
+			}
+		};
+		addField(enabledEditor);
+		dependentFields.add(
+				createBooleanField(REFACTORING_DELETE_COMPILED, "Delete compiled (pdf, midi) when deleting source"));
+		dependentFields.add(
+				createBooleanField(REFACTORING_WARN_DELETE_INCLUDED, "Warn if an indluded file is deleted"));
+		dependentFields.add(
+				createBooleanField(REFACTORING_WARN_SEARCHPATH, "Warn if a search path is affected by a refactoring"));
+		dependentFields.add(
+				createBooleanField(REFACTORING_WARN_VARIABLE_INCLUDE, "Warn if a there are variable includes (refactoring not supported)"));
+	}
+
+	private BooleanFieldEditor createBooleanField(String key, String fieldCaption) {
+		BooleanFieldEditor editor = new BooleanFieldEditor(key, fieldCaption, getFieldEditorParent());
+		addField(editor);
+		return editor;
+	}
+
+	private void setEnablement(List<BooleanFieldEditor> editors, boolean enabled) {
+		for (BooleanFieldEditor editor : editors) {
+			editor.setEnabled(enabled, getFieldEditorParent());
+		}
 	}
 }
